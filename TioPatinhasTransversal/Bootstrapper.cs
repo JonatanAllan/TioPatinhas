cd3b2232b95d42c06e1b702dde8669d953c2c8a6
Allan Jonatan <allan.jonatan@connectparts.com.br>
2017-02-24 11:55:28 -0300

    Criação apresentação

--- TioPatinhasTransversal/Bootstrapper.cs
+++ TioPatinhasTransversal/Bootstrapper.cs
@@ -1,4 +1,7 @@
 ﻿using SimpleInjector;
+using TioPatinhasAplicacao.Interfaces.ServicosApp;
+using TioPatinhasAplicacao.ServicosApp;
+using TioPatinhasDominio.Interfaces.Repositorios;
 using TioPatinhasDominio.Interfaces.Servicos;
 using TioPatinhasDominio.Servicos;
 using TioPatinhasRecursos.Interfaces.ServicosExternos;
@@ -19,7 +22,18 @@ namespace TioPatinhasTransversal
             container.Register<IExemploServicosExternos, ExemploServicosExternos>(Lifestyle.Scoped);
 
             // Aplicacao
-            //container.Register<IExemploServicosApp, ExemploServicosApp>(Lifestyle.Scoped);
+            container.Register<IClasseServicosApp, ClasseServicosApp>(Lifestyle.Scoped);
+            container.Register<IEnderecoServicosApp, EnderecoServicosApp>(Lifestyle.Scoped);
+            container.Register<IFamiliaServicosApp, FamiliaServicosApp>(Lifestyle.Scoped);
+            container.Register<IFornecedorServicosApp, FornecedorServicosApp>(Lifestyle.Scoped);
+            container.Register<IGrupoServicosApp, GrupoServicosApp>(Lifestyle.Scoped);
+            container.Register<IMarcaServicosApp, MarcaServicosApp>(Lifestyle.Scoped);
+            container.Register<IPedidoItemServicosApp, PedidoItemServicosApp>(Lifestyle.Scoped);
+            container.Register<IPedidoServicosApp, PedidoServicosApp>(Lifestyle.Scoped);
+            container.Register<IPedidoStatusServicosApp, PedidoStatusServicosApp>(Lifestyle.Scoped);
+            container.Register<IProdutoFornecedorServicosApp, ProdutoFornecedorServicosApp>(Lifestyle.Scoped);
+            container.Register<IProdutoServicosApp, ProdutoServicosApp>(Lifestyle.Scoped);
+            container.Register<ISubGrupoProdutoServicosApp, SubGrupoProdutoServicosApp>(Lifestyle.Scoped);
 
             // Dominio
             container.Register<IClasseServicos, ClasseServicos>(Lifestyle.Scoped);
@@ -37,6 +51,18 @@ namespace TioPatinhasTransversal
 
             // Infra
             //container.Register<IExemploRepositorio, ExemploRepositorio>(Lifestyle.Scoped);
+            //container.Register<IClasseRepositorio, ClasseRepositorio>(Lifestyle.Scoped);
+            //container.Register<IEnderecoRepositorio, EnderecoRepositorio>(Lifestyle.Scoped);
+            //container.Register<IFamiliaRepositorio, FamiliaRepositorio>(Lifestyle.Scoped);
+            //container.Register<IFornecedorRepositorio, FornecedorRepositorio>(Lifestyle.Scoped);
+            //container.Register<IGrupoRepositorio, GrupoRepositorio>(Lifestyle.Scoped);
+            //container.Register<IMarcaRepositorio, MarcaRepositorio>(Lifestyle.Scoped);
+            //container.Register<IPedidoItemRepositorio, PedidoItemRepositorio>(Lifestyle.Scoped);
+            //container.Register<IPedidoRepositorio, PedidoRepositorio>(Lifestyle.Scoped);
+            //container.Register<IPedidoStatusRepositorio, PedidoStatusRepositorio>(Lifestyle.Scoped);
+            //container.Register<IProdutoFornecedorRepositorio, ProdutoFornecedorRepositorio>(Lifestyle.Scoped);
+            //container.Register<IProdutoRepositorio, ProdutoRepositorio>(Lifestyle.Scoped);
+            //container.Register<ISubGrupoProdutoRepositorio, SubGrupoProdutoRepositorio>(Lifestyle.Scoped);
         }
     }
 }
